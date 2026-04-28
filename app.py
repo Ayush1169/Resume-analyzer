4bb239b1983b08fdd095f8f00242daf341abbec7
Ayush Agrawal <2305774@kiit.ac.in>
2026-04-28 13:13:22 +0530

    deploy setup added

--- app.py
+++ app.py
@@ -66,5 +66,7 @@ def download_pdf():
     return send_file("report.pdf", as_attachment=True)
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
